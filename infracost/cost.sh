c835267fede01c699cc7a5e6d535e1a3059da542
Avinash Varma <avinash@rafay.co>
2023-04-12 23:41:44 +0530

    adds infracost hook image

---
+++ infracost/cost.sh
@@ -0,0 +1,8 @@
+env
+cd /tmp
+curl -H "X-Engine-Helper-Token:$DOWNLOAD_TOKEN" -o ./job.tar.zst $DOWNLOAD_URL
+unzstd ./job.tar.zst
+mkdir terraform
+tar -xf job.tar -C ./terraform
+ls -al ./terraform
+infracost breakdown --path ./terraform
