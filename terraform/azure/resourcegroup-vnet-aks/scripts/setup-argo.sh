0945e3f14e315e08a0f3d43342ffe32c6c70a889
kutumba <kutumba@rafay.co>
2023-09-11 11:42:52 -0700

    update argo

--- terraform/azure/resourcegroup-vnet-aks/scripts/setup-argo.sh
+++ terraform/azure/resourcegroup-vnet-aks/scripts/setup-argo.sh
@@ -5,5 +5,5 @@ mv argocd-linux-amd64 argocd
 ./argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD --config /tmp/argo-config
 ./argocd cluster add --kubeconfig /tmp/kubeconfig $CLUSTER_NAME --yes --grpc-web --config /tmp/argo-config
 ./argocd app create $APP_NAME --repo https://github.com/RafaySystems/demo-apps --path Helm/webserver --dest-name $CLUSTER_NAME --dest-namespace $APP_NAMESPACE --grpc-web --config /tmp/argo-config
-./argocd app sync argo-demo --grpc-web --config /tmp/argo-config
-./argocd app set argo-demo --sync-policy automated --grpc-web --config /tmp/argo-config
+./argocd app sync  $APP_NAME --grpc-web --config /tmp/argo-config
+./argocd app set  $APP_NAME --sync-policy automated --grpc-web --config /tmp/argo-config
