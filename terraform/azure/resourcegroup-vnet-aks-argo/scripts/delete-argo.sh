2e64de9af242244776f26a032bef55844d5d7f5d
kutumba <kutumba@rafay.co>
2023-09-11 22:31:28 -0700

    updates to delete

--- terraform/azure/resourcegroup-vnet-aks-argo/scripts/delete-argo.sh
+++ terraform/azure/resourcegroup-vnet-aks-argo/scripts/delete-argo.sh
@@ -9,4 +9,4 @@ chmod +x argocd-linux-amd64
 mv argocd-linux-amd64 argocd
 ./argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD --config /tmp/argo-config
 ./argocd app delete $APP_NAME --grpc-web --config /tmp/argo-config
-./argocd cluster rm $CLUSTER_NAME --yes --grpc-web --config /tmp/argo-config
+./argocd cluster rm $CLUSTER_NAME --yes --grpc-web --config /tmp/argo-config --yes
