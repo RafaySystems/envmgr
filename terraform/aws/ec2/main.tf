39047d18f25d8af7a50cfac379c91824ff3d06a1
nirav-rafay <93963752+nirav-rafay@users.noreply.github.com>
2023-09-12 18:45:11 +0530

    Update main.tf

--- terraform/aws/ec2/main.tf
+++ terraform/aws/ec2/main.tf
@@ -23,3 +23,19 @@ resource "aws_instance" "ubuntu" {
     email       = "nirav.parikh@rafay.co"
   }
 }
+
+resource "aws_instance" "ubuntu2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name = var.instance2_name
+    env = "dev"
+    email       = "nirav.parikh@rafay.co"
+  }
+  volume_tags = {
+    env = "dev"
+    email       = "nirav.parikh@rafay.co"
+  }
+}
