2ceab151a6471067700144dfab12316395858591
timrafay <91907442+timrafay@users.noreply.github.com>
2024-01-02 15:36:21 -0500

    Update variable.tf

--- terraform/aws/101-genai-eks/terraform/genai/variable.tf
+++ terraform/aws/101-genai-eks/terraform/genai/variable.tf
@@ -2,6 +2,10 @@ variable "cluster_name" {
   description = "name of the eks cluster"
 }
 
+variable "namespace" {
+  description = "namespace for the applications"
+}
+
 variable "project" {
   description = "name of the project where the cluster resides"
   type    = string
