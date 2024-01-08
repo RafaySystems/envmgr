777fa134d7674470179cadc84e3b6981209c77f2
timrafay <91907442+timrafay@users.noreply.github.com>
2024-01-08 10:44:03 -0500

    Update variables.tf

--- terraform/oci/101-caas-oke/terraform/repository/variables.tf
+++ terraform/oci/101-caas-oke/terraform/repository/variables.tf
@@ -4,12 +4,15 @@ variable "project" {
 
 variable "repo_name" {
   type = string
+  default = "opa-repo"
 }
 
 variable "endpoint" {
   type = string
+  default = "https://github.com/RafaySystems/getstarted.git"
 }
 
 variable "type" {
   type = string
+  default = "Git"
 }
