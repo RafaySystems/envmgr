536d432408acd5a46d5def293112bd1149f0092c
timrafay <tim@rafay.co>
2024-01-10 15:57:38 -0500

    remove opa

--- terraform/oci/101-caas-oke/terraform/opa-installation-profile/output.tf
+++
@@ -1,3 +0,0 @@
-output "group" {
-  value = resource.rafay_opa_installation_profile.opa-installation-profile.name
-}
--- terraform/oci/101-caas-oke/terraform/opa-policy/output.tf
+++
@@ -1,3 +0,0 @@
-output "opa_policy" {
-  value = resource.rafay_opa_policy.opa-policy.metadata[0].name
-}
