adb1e0ca3fcf8b1a4fd28d57e5b08d008b1d4fb1
lan-rafay <62924689+lan-rafay@users.noreply.github.com>
2024-01-15 14:26:11 -0800

    Update delete.sh to handle wget overwritten RCTL download file if it exists

--- terraform/mks/101-caas-pnap/terraform/mks/delete.sh
+++ terraform/mks/101-caas-pnap/terraform/mks/delete.sh
@@ -5,7 +5,7 @@ set -ex
 RCTL_FILE="rctl-linux-amd64.tar.bz2"
 RCTL_URL="https://rafay-prod-cli.s3-us-west-2.amazonaws.com/publish/rctl-linux-amd64.tar.bz2"
 
-wget $RCTL_URL
+wget $RCTL_URL -O $RCTL_FILE
 if [ $? -eq 0 ];
 then
     echo "[+] Successfully Downloaded RCTL binary"
