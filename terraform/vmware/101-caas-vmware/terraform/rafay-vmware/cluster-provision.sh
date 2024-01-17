3e159da37b719e0f447b137b4a464bb18be2fab8
lan-rafay <62924689+lan-rafay@users.noreply.github.com>
2024-01-16 21:10:09 -0800

    Update cluster-provision.sh to handle overwrite RCTL file

--- terraform/vmware/101-caas-vmware/terraform/rafay-vmware/cluster-provision.sh
+++ terraform/vmware/101-caas-vmware/terraform/rafay-vmware/cluster-provision.sh
@@ -11,7 +11,7 @@ chmod +x ./jq
 RCTL_FILE="rctl-linux-amd64.tar.bz2"
 RCTL_URL="https://rafay-prod-cli.s3-us-west-2.amazonaws.com/publish/rctl-linux-amd64.tar.bz2"
 
-wget $RCTL_URL
+wget $RCTL_URL -O $RCTL_FILE
 if [ $? -eq 0 ]; then
     echo "[+] Successfully Downloaded RCTL binary"
 fi
