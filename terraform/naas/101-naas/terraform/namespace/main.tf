34258375a10ec4973e8e48dd90cdd3222273ebad
timrafay <tim@rafay.co>
2024-03-08 15:35:37 -0500

    add ztka

--- terraform/naas/101-naas/terraform/namespace/main.tf
+++ terraform/naas/101-naas/terraform/namespace/main.tf
@@ -43,32 +43,6 @@ resource "rafay_namespace" "namespace" {
   }
 
 
-resource "rafay_group" "group" {
-  name        = "${local.namespace}-group"
-}
-
-resource "rafay_groupassociation" "groupassociation" {
-  depends_on = [rafay_group.group]
-  project = "${var.project}"
-  group = "${local.namespace}-group"
-  namespaces = ["${local.namespace}"]
-  roles = ["NAMESPACE_ADMIN"]
-  add_users = ["${var.username}"]
-  idp_user = var.user_type
-}
-
-
-resource "rafay_groupassociation" "groupassociation_collaborators" {
-  count = var.collaborator == "user_email" ? 0 : 1
-  depends_on = [rafay_groupassociation.groupassociation]
-  project = "${var.project}"
-  roles = ["NAMESPACE_ADMIN"]
-  group = "${local.namespace}-group"
-  namespaces = ["${local.namespace}"]
-  add_users = ["${var.collaborator}"]
-  idp_user = true
-}
-
 resource "rafay_download_kubeconfig" "tfkubeconfig" {
   cluster            = var.cluster_name
   output_folder_path = "/tmp"
@@ -88,4 +62,98 @@ resource "null_resource" "install_network_policy" {
     command = "wget \"https://dl.k8s.io/release/$(wget --output-document - --quiet https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod +x ./kubectl && ./kubectl apply -f /tmp/networkpolicy.yaml -n ${local.namespace} --kubeconfig=/tmp/kubeconfig"
   }
   depends_on = [local_file.create_network_policy]
+}
+
+resource "local_file" "create_ztka_rule" {
+  content  = templatefile("rule.yaml", {namespace = local.namespace })
+  filename = "/tmp/ztkarule.yaml"
+}
+
+resource "rafay_ztkarule" "ztkarule" 
+  depends_on = [local_file.create_ztka_rule]
+  metadata {
+    name = "ztkarule-network-policy"
+  }
+  spec {
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://tmp/ztkarule.yaml"
+        }
+      }
+      options {
+        force                       = true
+        disable_open_api_validation = true
+      }
+    }
+    cluster_selector {
+      select_all = true
+    }
+    project_selector {
+      match_names = [
+        "var.project"
+      ]
+      select_all = false
+    }
+    version = "v1"
+  }
+}
+
+resource "rafay_ztkapolicy" "rafay_ztkapolicy" {
+  depends_on = [rafay_ztkarule.ztkarule]
+  metadata {
+    name = "ztkapolicy-network-policy"
+  }
+  spec {
+    ztka_rule_list {
+      name    = "ztkarule-network-policy"
+      version = "v1"
+    }
+    version = "v1"
+  }
+}
+
+resource "rafay_customrole" "rafay_customrole" {
+  depends_on = [rafay_ztkapolicy.rafay_ztkapolicy]
+  metadata {
+    name = "customrole-network-policy"
+  }
+  spec {
+    ztka_policy_list {
+      name    = "ztkapolicy-network-policy"
+      version = "v1"
+    }
+    base_role = "NAMESPACE_ADMIN"
+  }
+}
+
+
+resource "rafay_group" "group" {
+  depends_on = [rafay_customrole.rafay_customrole]
+  name        = "${local.namespace}-group"
+}
+
+resource "rafay_groupassociation" "groupassociation" {
+  depends_on = [rafay_group.group]
+  project = "${var.project}"
+  group = "${local.namespace}-group"
+  namespaces = ["${local.namespace}"]
+  roles = ["NAMESPACE_ADMIN"]
+  custom_roles = ["customrole-network-policy"]
+  add_users = ["${var.username}"]
+  idp_user = var.user_type
+}
+
+
+resource "rafay_groupassociation" "groupassociation_collaborators" {
+  count = var.collaborator == "user_email" ? 0 : 1
+  depends_on = [rafay_groupassociation.groupassociation]
+  project = "${var.project}"
+  roles = ["NAMESPACE_ADMIN]
+  custom_roles = ["customrole-network-policy"]
+  group = "${local.namespace}-group"
+  namespaces = ["${local.namespace}"]
+  add_users = ["${var.collaborator}"]
+  idp_user = true
 }
