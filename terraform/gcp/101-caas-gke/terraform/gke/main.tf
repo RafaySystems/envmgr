8c201cbf05d0b335f64268e48959cdcfc36c624f
Hardik Italia <hardik@rafay.co>
2024-03-18 23:25:29 -0700

    Updated rafay-gke example

--- terraform/gcp/101-caas-gke/terraform/gke/main.tf
+++ terraform/gcp/101-caas-gke/terraform/gke/main.tf
@@ -3,7 +3,6 @@ resource "random_integer" "networkbit" {
   max = 4095
 }
 
-
 resource "rafay_gke_cluster" "tf-example" {
   metadata {
     name    = var.cluster_name
@@ -20,27 +19,49 @@ resource "rafay_gke_cluster" "tf-example" {
       gcp_project           = var.gcp_project
       control_plane_version = var.k8s_version
       location {
-        type = "zonal"
+        type = var.region != "" ? "regional" : "zonal"
         config {
-          zone = var.zone
+          region = try(var.region, null)
+          zone   = var.zone
         }
       }
+      security {
+        enable_google_groups_for_rbac = try(var.enable_google_groups_for_rbac, null)
+        enable_legacy_authorization   = var.enable_legacy_authorization
+        enable_workload_identity      = var.enable_workload_identity
+        issue_client_certificate      = var.issue_client_certificate
+      }
       network {
-        name                     = var.network
-        subnet_name              = var.subnet
-        enable_vpc_nativetraffic = "true"
-        max_pods_per_node        = 110
+        name                         = var.network
+        subnet_name                  = var.subnet
+        enable_vpc_nativetraffic     = var.enable_vpc_nativetraffic
+        max_pods_per_node            = var.max_pods_per_node
+        pod_address_range            = try(var.pod_address_range, null)
+        service_address_range        = try(var.service_address_range, null)
+        pod_secondary_range_name     = try(var.pod_secondary_range_name, null)
+        service_secondary_range_name = try(var.service_secondary_range_name, null)
+
         access {
-          type = "private"
+          type = var.access_type
           config {
-            #ontrol_plane_ip_range                  = var.control_plane_ip_range
             control_plane_ip_range                  = cidrsubnet(var.control_plane_ip_range, 12, random_integer.networkbit.result)
             enable_access_control_plane_external_ip = true
           }
         }
       }
       features {
-        enable_compute_engine_persistent_disk_csi_driver = "true"
+        enable_compute_engine_persistent_disk_csi_driver = var.enable_compute_engine_persistent_disk_csi_driver ? true : false
+        enable_filestore_csi_driver                      = var.enable_filestore_csi_driver ? true : false
+        enable_application_manager_beta                  = var.enable_application_manager_beta ? true : false
+        enable_backup_for_gke                            = var.enable_backup_for_gke ? true : false
+        enable_image_streaming                           = var.enable_image_streaming ? true : false
+        enable_managed_service_prometheus                = var.enable_managed_service_prometheus ? true : false
+        enable_cloud_logging                             = var.enable_cloud_logging ? true : false
+        cloud_logging_components                         = var.enable_cloud_logging ? var.cloud_logging_components : null
+        enable_cloud_monitoring                          = var.enable_cloud_monitoring ? true : false
+        cloud_monitoring_components                      = var.enable_cloud_monitoring ? var.cloud_monitoring_components : null
+
+
       }
       dynamic "node_pools" {
         for_each = var.node_pools
@@ -50,13 +71,35 @@ resource "rafay_gke_cluster" "tf-example" {
           size         = node_pools.value.node_size
           machine_config {
             machine_type   = node_pools.value.machine_type
-            image_type     = "COS_CONTAINERD"
-            boot_disk_type = "pd-standard"
-            boot_disk_size = 100
+            image_type     = node_pools.value.image_type
+            boot_disk_type = node_pools.value.disk_type
+            boot_disk_size = node_pools.value.disk_size
           }
           auto_scaling {
             max_nodes = node_pools.value.max_nodes
             min_nodes = node_pools.value.min_nodes
+          }
+          networking {
+            max_pods_per_node = node_pools.value.max_pods_per_node
+            network_tags      = try(node_pools.value.network_tags, null)
+
+          }
+          metadata {
+            dynamic "kubernetes_labels" {
+              for_each = node_pools.value.kubernetes_labels == null ? {} : node_pools.value.kubernetes_labels
+              content {
+                key   = kubernetes_labels.key
+                value = kubernetes_labels.value
+              }
+            }
+            dynamic "node_taints" {
+              for_each = node_pools.value.node_taints == null ? [] : [node_pools.value.node_taints]
+              content {
+                effect = node_taints.value.effect
+                key    = node_taints.value.key
+                value  = node_taints.value.value
+              }
+            }
           }
         }
       }
--- terraform/gcp/101-caas-gke/terraform/gke/variables.tf
+++ terraform/gcp/101-caas-gke/terraform/gke/variables.tf
@@ -17,13 +17,27 @@ variable "blueprint_version" {
 }
 
 variable "credentials_name" {
-  type    = string
-  default = "gke-credentials"
+  type = string
+}
+
+## Requied for Regional cluster
+variable "region" {
+  type = string
+  #default = "us-central1"
 }
 
 variable "zone" {
   type    = string
-  default = "us-central1-c"
+  default = "us-central1-a"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.28"
+}
+
+variable "gcp_project" {
+  type = string
 }
 
 variable "network" {
@@ -36,33 +50,73 @@ variable "subnet" {
   default = "default"
 }
 
-variable "k8s_version" {
-  type    = string
-  default = "1.26"
+variable "max_pods_per_node" {
+  type    = number
+  default = 110
 }
 
-variable "gcp_project" {
-  type = string
+variable "enable_vpc_nativetraffic" {
+  type    = bool
+  default = true
+
+}
+
+variable "pod_address_range" {
+  type    = string
+  default = ""
+}
+
+variable "service_address_range" {
+  type    = string
+  default = ""
+}
+
+
+variable "pod_secondary_range_name" {
+  type    = string
+  default = ""
+}
+
+variable "service_secondary_range_name" {
+  type    = string
+  default = ""
 }
 
 variable "node_pools" {
   type = map(object({
-    name         = string
-    node_size    = number
-    max_nodes    = number
-    min_nodes    = number
-    node_version = string
-    machine_type = string
+    name              = string
+    node_size         = number
+    max_nodes         = number
+    min_nodes         = number
+    node_version      = string
+    machine_type      = string
+    image_type        = string
+    disk_size         = number
+    disk_type         = string
+    max_pods_per_node = number
+    network_tags      = list(string)
+    kubernetes_labels = optional(map(string))
+    node_taints = optional(object({
+      key    = string
+      value  = string
+      effect = string
+    }))
 
   }))
   default = {
     "pool1" = {
-      name         = "pool1"
-      node_size    = 1
-      min_nodes    = 1
-      max_nodes    = 3
-      node_version = "1.26"
-      machine_type = "e2-standard-4"
+      name              = "pool1"
+      node_size         = 1
+      min_nodes         = 1
+      max_nodes         = 3
+      node_version      = "1.28"
+      machine_type      = "e2-standard-4"
+      image_type        = "COS_CONTAINERD"
+      disk_size         = 100
+      disk_type         = "pd-standard"
+      max_pods_per_node = 110
+      network_tags      = null
+      kubernetes_labels = { "managed-by" : "Rafay" }
     }
   }
 }
@@ -70,4 +124,88 @@ variable "node_pools" {
 variable "control_plane_ip_range" {
   type    = string
   default = "172.16.0.0/16"
+}
+
+## Valid values are `private` & `public`
+variable "access_type" {
+  type    = string
+  default = "private"
+}
+
+## Set value to true if authorized_network is not empty.
+variable "enabled_access" {
+  type    = bool
+  default = true
+}
+
+variable "enable_compute_engine_persistent_disk_csi_driver" {
+  type    = bool
+  default = true
+}
+
+## if set to true check value for `cloud_monitoring_components`
+variable "enable_cloud_monitoring" {
+  type    = bool
+  default = false
+}
+
+## Update value as per requirements when `enable_cloud_monitoring` is set to true
+variable "cloud_monitoring_components" {
+  type    = list(string)
+  default = ["SYSTEM_COMPONENTS"]
+}
+
+## if set to true check value for `cloud_logging_components`
+variable "enable_cloud_logging" {
+  type    = bool
+  default = false
+}
+
+## Update value as per requirements when `enable_cloud_logging` is set to true
+variable "cloud_logging_components" {
+  type    = list(string)
+  default = ["SYSTEM_COMPONENTS", "WORKLOADS"]
+}
+
+variable "enable_application_manager_beta" {
+  type    = bool
+  default = false
+}
+
+variable "enable_backup_for_gke" {
+  type    = bool
+  default = false
+}
+
+variable "enable_filestore_csi_driver" {
+  type    = bool
+  default = false
+}
+
+variable "enable_image_streaming" {
+  type    = bool
+  default = false
+}
+
+variable "enable_managed_service_prometheus" {
+  type    = bool
+  default = false
+}
+
+variable "enable_google_groups_for_rbac" {
+  type    = bool
+  default = false
+}
+
+variable "enable_legacy_authorization" {
+  type    = bool
+  default = false
+}
+variable "enable_workload_identity" {
+  type    = bool
+  default = false
+}
+variable "issue_client_certificate" {
+  type    = bool
+  default = false
 }
