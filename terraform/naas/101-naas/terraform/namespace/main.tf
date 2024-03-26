7c71f1836f99e2748a24c5655e6dcfacc446338f
timrafay <91907442+timrafay@users.noreply.github.com>
2024-03-26 14:47:05 -0400

    Update main.tf

--- terraform/naas/101-naas/terraform/namespace/main.tf
+++ terraform/naas/101-naas/terraform/namespace/main.tf
@@ -48,6 +48,9 @@ resource "rafay_namespace" "namespace" {
 
 
 resource "rafay_download_kubeconfig" "tfkubeconfig" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   cluster            = var.cluster_name
   output_folder_path = "/tmp"
   filename           = "kubeconfig"
