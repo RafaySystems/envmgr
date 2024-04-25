d5c2d0fc13034472861a3a903eca30047d64d49b
timrafay <91907442+timrafay@users.noreply.github.com>
2024-04-25 10:56:19 -0400

    Update main.tf

--- terraform/aws/101-vpc-ec2/terraform/main.tf
+++ terraform/aws/101-vpc-ec2/terraform/main.tf
@@ -4,6 +4,14 @@ resource "random_string" "resource_code" {
   upper   = false
 }
 
+locals {
+ instance_type = var.compute == "2 vCPU, 2 GiB Memory" ? t3.small : false
+ instance_type = var.compute == "2 vCPU, 4 GiB Memory" ? t3.medium : false
+ instance_type = var.compute == "2 vCPU, 8 GiB Memory" ? t3.large : false
+ instance_type = var.compute == "4 vCPU, 16 GiB Memory" ? t3.xlarge : false
+ instance_type = var.compute == "8 vCPU, 32 GiB Memory" ? t3.2xlarge : false
+}
+
 module "vpc" {
   source     = "./modules/vpc"
   vpc_cidr   = var.vpc_cidr
@@ -13,7 +21,7 @@ module "vpc" {
 
 module "ec2" {
   source        = "./modules/ec2"
-  instance_type = var.instance_type
+  instance_type = local.instance_type
   subnet_id     = module.vpc.private_subnets[0]
   prefix        = random_string.resource_code.result
 }
