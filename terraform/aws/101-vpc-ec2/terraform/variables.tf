fdf478f6e606e6f6587fd5cd88f86f2f8a7f8dd2
timrafay <91907442+timrafay@users.noreply.github.com>
2024-04-25 11:26:04 -0400

    Update variables.tf

--- terraform/aws/101-vpc-ec2/terraform/variables.tf
+++ terraform/aws/101-vpc-ec2/terraform/variables.tf
@@ -28,3 +28,30 @@ variable "network" {
   description = "subnet name"
   type        = string
 }
+
+variable "isntance_map" {
+  type = "list"
+  description = "list of instance types
+  default = [
+      {
+        id = "2 vCPU, 2 GiB Memory"
+        attribute = "t3.small"
+      },
+      {
+        id = "2 vCPU, 4 GiB Memory"
+        attribute = "t3.medium"
+      },
+      {
+        id = "2 vCPU, 8 GiB Memory"
+        attribute = "t3.large"
+      },
+      {
+        id = "4 vCPU, 16 GiB Memory"
+        attribute = "t3.xlarge"
+      },
+      {
+        id = "8 vCPU, 32 GiB Memory"
+        attribute = "t3.2xlarge"
+      }
+  ]
+}
