60e142c55220fc1aee5704aece9c17da35d64f14
timrafay <91907442+timrafay@users.noreply.github.com>
2024-04-25 18:11:56 -0400

    Update output.tf

--- terraform/aws/101-vpc-ec2/terraform/modules/ec2/output.tf
+++ terraform/aws/101-vpc-ec2/terraform/modules/ec2/output.tf
@@ -2,6 +2,13 @@ output "ip_address"{
   value = module.ec2_instance.private_ip
 }
 
+data "aws_instance" "ec2_instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["instance-name-tag"]
+  }
+}
+
 output "instance_id"{
-  value = module.ec2_instance.instance_id
+  value = data.aws_instance.ec2_instance.instance_id
 }
