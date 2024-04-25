d6b47784fe6000ccc0bacdaf1d2131bd301e5480
timrafay <91907442+timrafay@users.noreply.github.com>
2024-04-25 18:00:32 -0400

    Update output.tf

--- terraform/aws/101-vpc-ec2/terraform/output.tf
+++ terraform/aws/101-vpc-ec2/terraform/output.tf
@@ -6,3 +6,7 @@ output "vpc_id" {
 output "instance_ip" {
   value = module.ec2.ip_address
 }
+
+output "instance_url" {
+  value = "https://${var.aws_region}.console.aws.amazon.com/ec2/home?region=us-west-2#InstanceDetails:instanceId=${module.ec2.instance_id}"
+}
