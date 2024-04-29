99c9dcad517bd77a4734f4ba332d66a3bc4d73c7
Hardik Italia <hardik@rafay.co>
2024-04-29 00:56:13 -0700

    Shared Cluster Single GPU template

---
+++ terraform/naas/103-naas-single-gpu/terraform/variables.tf
@@ -0,0 +1,47 @@
+variable "project" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "annotations" {
+  type    = map(string)
+  default = {}
+}
+
+variable "namespace_quota_size" {
+  type    = string
+  default = "single_gpu"
+}
+
+variable "namespace_quotas" {
+  default = {
+    single_gpu = {
+      cpu_requests    = "2600m"
+      memory_requests = "234Gi"
+      cpu_limits      = "2600m"
+      memory_limits   = "234Gi"
+      gpu_requests    = "1"
+      gpu_limits      = "1"
+    }
+  }
+}
+
+# variable "group" {
+#   type = string
+# }
+
+# variable "user" {
+#   type = string
+# }
