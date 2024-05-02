48023b9cc3a68783dd219f995409b483c59fe66e
kutumba <kutumba@rafay.co>
2024-05-01 19:18:26 -0700

    updates to gke

--- terraform/gcp/caas-gke/main.tf
+++ terraform/gcp/caas-gke/main.tf
@@ -140,22 +140,22 @@ data "google_client_config" "current" {}
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  #token                  = data.google_client_config.current.access_token
-  exec {
+  token                  = data.google_client_config.current.access_token
+  /*exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command = "gke-gcloud-auth-plugin"
-  }
+  }*/
 }
 
 provider "helm" {
   kubernetes {
     host                   = "https://${google_container_cluster.primary.endpoint}"
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-    #token                  = data.google_client_config.current.access_token
-    exec {
+    token                  = data.google_client_config.current.access_token
+    /*exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command = "gke-gcloud-auth-plugin"
-    }
+    }*/
   }
 }
 
