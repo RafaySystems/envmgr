467c96fa53b92e09859e0e71234c9bb0d2988325
kutumba <kutumba@rafay.co>
2024-05-01 17:40:23 -0700

    updates to gke tf

--- terraform/gcp/caas-gke/main.tf
+++ terraform/gcp/caas-gke/main.tf
@@ -140,14 +140,22 @@ data "google_client_config" "current" {}
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  token                  = data.google_client_config.current.access_token
+  #token                  = data.google_client_config.current.access_token
+  exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command = "gke-gcloud-auth-plugin"
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = "https://${google_container_cluster.primary.endpoint}"
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-    token                  = data.google_client_config.current.access_token
+    #token                  = data.google_client_config.current.access_token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command = "gke-gcloud-auth-plugin"
+    }
   }
 }
 
@@ -178,7 +186,7 @@ resource "helm_release" "rafay_operator" {
   }
 }
 
-data "rafay_download_kubeconfig" "kubeconfig_cluster" {
+/*data "rafay_download_kubeconfig" "kubeconfig_cluster" {
   username = var.username
   depends_on = [helm_release.rafay_operator]
 }
@@ -188,4 +196,4 @@ data "rafay_download_kubeconfig" "kubeconfig_cluster" {
 output "kubeconfig_cluster" {
   description = "kubeconfig_cluster"
   value       = data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig
-}
+}*/
