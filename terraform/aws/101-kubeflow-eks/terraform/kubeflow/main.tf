ca242504da30bfadab2e47469a3c82c031c21cfc
timrafay <91907442+timrafay@users.noreply.github.com>
2024-05-03 17:13:42 -0400

    Update main.tf

--- terraform/aws/101-kubeflow-eks/terraform/kubeflow/main.tf
+++ terraform/aws/101-kubeflow-eks/terraform/kubeflow/main.tf
@@ -46,7 +46,7 @@ resource "null_resource" "kubeflow_install" {
   depends_on = [null_resource.clone_git]
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command     = "while ! kustomize build example | ./kubectl apply --kubeconfig=/tmp/kubeconfig -f -; do echo "Retrying to apply resources"; sleep 10; done "
+    command     = "while ! kustomize build example | ./kubectl apply --kubeconfig=/tmp/kubeconfig -f -; do echo Retrying; sleep 10; done "
   }
 }
 
