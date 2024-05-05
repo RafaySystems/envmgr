960aad80a673dc1c80f783d28e1106c393b1c620
timrafay <91907442+timrafay@users.noreply.github.com>
2024-05-05 13:07:10 -0400

    Update output.tf

--- terraform/aws/101-kubeflow-eks/terraform/kubeflow/output.tf
+++ terraform/aws/101-kubeflow-eks/terraform/kubeflow/output.tf
@@ -1,3 +1,10 @@
 output "public_ip" {
-  value = "http://${data.local_file.kubeflow-ip.content}:3000"
+  value = "http://${data.local_file.kubeflow-ip.content}:8080"
+}
+
+output "default-username" {
+  value = "user@example.com"
+}
+output "default-password" {
+  value = "12341234"
 }
