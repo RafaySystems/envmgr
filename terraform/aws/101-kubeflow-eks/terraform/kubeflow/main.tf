a7cff6312065abd3999e70b938be5bee3f71310a
timrafay <91907442+timrafay@users.noreply.github.com>
2024-05-05 13:52:33 -0400

    Update main.tf

--- terraform/aws/101-kubeflow-eks/terraform/kubeflow/main.tf
+++ terraform/aws/101-kubeflow-eks/terraform/kubeflow/main.tf
@@ -6,38 +6,11 @@ resource "rafay_download_kubeconfig" "tfkubeconfig" {
   filename           = "kubeconfig"
 }
 
-resource "null_resource" "kubectl_install" {
-  triggers = {
-    always_run = timestamp()
-  }
-  depends_on = [rafay_download_kubeconfig.tfkubeconfig]
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "wget \"https://dl.k8s.io/release/$(wget --output-document - --quiet https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod +x ./kubectl && ls /tmp"
-  }
-}
 
-resource "null_resource" "clone_git" {
-  triggers = {
-    always_run = timestamp()
-  }
-  depends_on = [null_resource.kubectl_install]
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "git clone https://github.com/kubeflow/manifests.git"
-  }
-}
 
-resource "null_resource" "kubeflow_install" {
-  triggers = {
-    always_run = timestamp()
-  }
-  depends_on = [null_resource.clone_git]
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "cd manifests && while ! kustomize build example | ../kubectl apply --kubeconfig=/tmp/kubeconfig -f -; do echo Retrying; sleep 10; done "
-  }
-}
+
+
+
 
 resource "null_resource" "lb_install" {
   triggers = {
@@ -46,7 +19,7 @@ resource "null_resource" "lb_install" {
   depends_on = [null_resource.kubeflow_install]
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command     = "./kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io --kubeconfig=/tmp/kubeconfig  && ./kubectl expose deployment istio-ingressgateway --type=LoadBalancer --name=kubeflow-ui-loadbalancer -n istio-system --kubeconfig=/tmp/kubeconfig "
+    command     = "./kubectl expose deployment istio-ingressgateway --type=LoadBalancer --name=kubeflow-ui-loadbalancer -n istio-system --kubeconfig=/tmp/kubeconfig "
   }
 }
 
@@ -59,7 +32,7 @@ resource "time_sleep" "wait_60_seconds" {
 resource "null_resource" "get_kubeflow_ip" {
   triggers  =  { always_run = "${timestamp()}" }
   provisioner "local-exec" {
-    command = "./kubectl get svc kubeflow-ui-loadbalancer -n kubeflow --kubeconfig=/tmp/kubeconfig | awk -F' ' '{print $4}' | tail -1 | tr -d '\n' >> /tmp/kubeflow_ip.txt"
+    command = "./kubectl get svc kubeflow-ui-loadbalancer -n istio-system --kubeconfig=/tmp/kubeconfig | awk -F' ' '{print $4}' | tail -1 | tr -d '\n' >> /tmp/kubeflow_ip.txt"
   }
   depends_on = [time_sleep.wait_60_seconds]
 }
