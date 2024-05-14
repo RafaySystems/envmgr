77aad349c10e321e8e4e5e97a46bbc6c8536cce3
Hardik Italia <hardik@rafay.co>
2024-05-14 13:41:35 -0700

    Updated vpc module example

--- terraform/aws/vpc/variables.tf
+++ terraform/aws/vpc/variables.tf
@@ -1,13 +1,26 @@
 variable "region" {
-  default     = "us-west-2"
   description = "AWS region"
+  type        = string
+  default     = "us-west-2"
 }
 
-variable "name" {
-  default     = "envmgr-demo-vpc"
-  description = "vpc name"
+variable "vpc_cidr" {
+  description = "CIDR block for main"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "email" {
-  description = "Email tag to be added while creating the resource"
+variable "vpc_name" {
+  description = "VPC NAME"
+  default     = "example-vpc"
+  type        = string
+}
+
+
+variable "tags" {
+  description = "AWS Tags"
+  type        = map(string)
+  default = {
+    "cluster-name" = "demo-cluster"
+  }
 }
