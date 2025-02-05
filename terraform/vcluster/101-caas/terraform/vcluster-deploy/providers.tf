027516aaa3e52910e4c18d8d6c7ec64b3c81a7e4
timrafay <91907442+timrafay@users.noreply.github.com>
2025-02-05 13:22:48 -0500

    Update providers.tf

--- terraform/vcluster/101-caas/terraform/vcluster-deploy/providers.tf
+++ terraform/vcluster/101-caas/terraform/vcluster-deploy/providers.tf
@@ -1,8 +1,28 @@
 terraform {
   required_providers {
     rafay = {
-      version = ">= 0.1"
-      source  = "registry.terraform.io/RafaySystems/rafay"
+      source = "rafay/rafay"
+      version = "1.1.38"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.16.1"
+    }
+    kubernetes = {
+      source = "opentofu/kubernetes"
+      version = "2.33.0"
+    }
+    htpasswd = {
+      source = "loafoe/htpasswd"
+      version = "1.2.1"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
     }
   }
 }
