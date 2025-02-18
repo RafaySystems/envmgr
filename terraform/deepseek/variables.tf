e875b2a0ea9873f9d53907bf951fadf436af73e7
timrafay <91907442+timrafay@users.noreply.github.com>
2025-02-18 10:59:42 -0500

    Update variables.tf

--- terraform/deepseek/variables.tf
+++ terraform/deepseek/variables.tf
@@ -1,24 +1,4 @@
-variable "region" {
-  type        = string
-  default = "us-west-2"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "Name of the cluster"
-}
-
 variable "project_name" {
   type        = string
   description = "Name of the project"
-}
-
-variable "blueprint" {
-  type        = string
-  description = "Blueprint to use"
-}
-
-variable "blueprint_version" {
-  type        = string
-  description = "Version of the blueprint"
 }
