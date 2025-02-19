ac85a316c1aa3dde2af57fd76becd6b31a215d19
timrafay <91907442+timrafay@users.noreply.github.com>
2025-02-18 20:01:19 -0500

    Update main.tf

--- terraform/deepseek/main.tf
+++ terraform/deepseek/main.tf
@@ -19,7 +19,7 @@ variable "enable_auto_mode_node_pool" {
 locals {
   region   = "us-west-2"
   vpc_cidr = "10.0.0.0/16"
-  name     = "eks-automode6"
+  name     = "eks-automode7"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
