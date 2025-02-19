6cbd9fffb834279b1b8b5276bfed8f209b90b7e0
timrafay <tim@rafay.co>
2025-02-19 09:07:33 -0500

    v1

--- terraform/deepseek/output/output.tf
+++ terraform/deepseek/output/output.tf
@@ -1,9 +1,11 @@
+locals {
+  name     = "eks-automode7"
+}
+
 data "rafay_download_kubeconfig" "kubeconfig_cluster" {
   cluster = local.name
-  depends_on = [null_resource.delete-webhook]
 }
 
 output "kubeconfig" {
   value = yamldecode(data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig)
-   #depends_on = [rafay_import_cluster.import_cluster]
 }
