ca20bb4d7ae54c98ebd36fb06b89759c6e078939
timrafay <tim@rafay.co>
2025-02-19 11:15:28 -0500

    Update helm.tf

--- terraform/deepseek/nodes/helm.tf
+++ terraform/deepseek/nodes/helm.tf
@@ -1,6 +1,48 @@
+provider "aws" {
+  region = "us-west-2" # Change to your desired region
+}
+
 data "aws_ecr_repository" "neuron-ecr" {
   name = "${var.name}-neuron-base"
 }
+
+data "aws_eks_cluster" "deepseek" {
+  name = var.name 
+}
+
+
+provider "kubernetes" {
+  #host                   = module.eks.cluster_endpoint
+  #cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.deepseek.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.deepseek.certificate_authority.0.data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.deepseek.name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    #host                   = module.eks.cluster_endpoint
+    #cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+	  host                   = data.aws_eks_cluster.deepseek.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.deepseek.certificate_authority.0.data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.deepseek.name]
+    }
+  }
+}
+
+
+
 
 
 resource "helm_release" "deepseek_gpu" {
