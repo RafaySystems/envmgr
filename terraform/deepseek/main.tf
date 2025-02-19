fb3348c5d1c245f98c12a647bb4a1af5f2186d14
timrafay <91907442+timrafay@users.noreply.github.com>
2025-02-18 19:21:49 -0500

    Update main.tf

--- terraform/deepseek/main.tf
+++ terraform/deepseek/main.tf
@@ -201,10 +201,10 @@ resource "null_resource" "delete-webhook" {
 
 data "rafay_download_kubeconfig" "kubeconfig_cluster" {
   cluster = local.name
-  depends_on = [rafay_import_cluster.import_cluster]
+  depends_on = [null_resource.delete-webhook]
 }
 
 output "kubeconfig" {
   value = yamldecode(data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig)
-    depends_on = [rafay_import_cluster.import_cluster]
+   #depends_on = [rafay_import_cluster.import_cluster]
 }
