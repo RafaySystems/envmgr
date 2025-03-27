ec524ee9916960931e872a3f93c161a67f32d42b
timrafay <91907442+timrafay@users.noreply.github.com>
2025-03-27 15:27:24 -0400

    Update output.tf

--- terraform/aws/101-genai-eks/terraform/genai/output.tf
+++ terraform/aws/101-genai-eks/terraform/genai/output.tf
@@ -4,9 +4,9 @@ output "namepsace" {
   value =local.namespace
 }
 
-output "public_ip_example1" {
-  value = "http://${data.local_file.gen-ai-ip-example1.content}:80"
-}
+#output "public_ip_example1" {
+#  value = "http://${data.local_file.gen-ai-ip-example1.content}:80"
+#}
 
 output "public_ip_example2" {
   value = "http://${data.local_file.gen-ai-ip-example2.content}:8000"
