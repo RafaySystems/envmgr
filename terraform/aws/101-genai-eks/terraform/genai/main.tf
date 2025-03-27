5362550dd5c129cbb4c24ba183f69e59536cb296
timrafay <91907442+timrafay@users.noreply.github.com>
2025-03-27 15:27:07 -0400

    Update main.tf

--- terraform/aws/101-genai-eks/terraform/genai/main.tf
+++ terraform/aws/101-genai-eks/terraform/genai/main.tf
@@ -23,19 +23,19 @@ resource "rafay_download_kubeconfig" "tfkubeconfig" {
   filename           = "kubeconfig"
 }
 
-resource "null_resource" "install_example1" {
-  triggers = {
-    always_run = timestamp()
-  }
-  depends_on = [
-   rafay_download_kubeconfig.tfkubeconfig,
-   time_sleep.wait_for_service_account
-]
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "wget \"https://dl.k8s.io/release/$(wget --output-document - --quiet https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod +x ./kubectl && ls /tmp && ./kubectl apply -f ./genai-app.yaml -n ${local.namespace} --kubeconfig=/tmp/kubeconfig && ./kubectl expose deployment gen-ai --type=LoadBalancer --name=gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig "
-  }
-}
+#resource "null_resource" "install_example1" {
+#  triggers = {
+#    always_run = timestamp()
+#  }
+#  depends_on = [
+#   rafay_download_kubeconfig.tfkubeconfig,
+#   time_sleep.wait_for_service_account
+#]
+#  provisioner "local-exec" {
+#    interpreter = ["/bin/bash", "-c"]
+#    command     = "wget \"https://dl.k8s.io/release/$(wget --output-document - --quiet https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod +x ./kubectl && ls /tmp && ./kubectl apply -f ./genai-app.yaml -n ${local.namespace} --kubeconfig=/tmp/kubeconfig && ./kubectl expose deployment gen-ai --type=LoadBalancer --name=gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig "
+#  }
+#}
 
 
 resource "rafay_workload" "install_example2" {
@@ -68,18 +68,18 @@ resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
 }
 
-resource "null_resource" "get-gen-ai-ip-example1" {
-  triggers  =  { always_run = "${timestamp()}" }
-  provisioner "local-exec" {
-    command = "./kubectl get svc gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig | awk -F' ' '{print $4}' | tail -1 | tr -d '\n' >> /tmp/gen-ai-ip-example1.txt"
-  }
-  depends_on = [time_sleep.wait_30_seconds]
-}
+#resource "null_resource" "get-gen-ai-ip-example1" {
+#  triggers  =  { always_run = "${timestamp()}" }
+#  provisioner "local-exec" {
+#    command = "./kubectl get svc gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig | awk -F' ' '{print $4}' | tail -1 | tr -d '\n' >> /tmp/gen-ai-ip-example1.txt"
+#  }
+#  depends_on = [time_sleep.wait_30_seconds]
+#}
 
-data "local_file" "gen-ai-ip-example1" {
-    filename = "/tmp/gen-ai-ip-example1.txt"
-  depends_on = [null_resource.get-gen-ai-ip-example1]
-}
+#data "local_file" "gen-ai-ip-example1" {
+#    filename = "/tmp/gen-ai-ip-example1.txt"
+#  depends_on = [null_resource.get-gen-ai-ip-example1]
+#}
 
 resource "null_resource" "get-gen-ai-ip-example2" {
   triggers  =  { always_run = "${timestamp()}" }
