4ac1d021c4bcce49bd547fdc0be27afea59a8f1b
timrafay <tim@rafay.co>
2025-03-28 15:03:13 -0400

    Update main.tf

--- terraform/aws/101-genai-eks/terraform/genai/main.tf
+++ terraform/aws/101-genai-eks/terraform/genai/main.tf
@@ -12,30 +12,12 @@ resource "null_resource" "create_secret" {
   }
 }
 
-resource "time_sleep" "wait_for_service_account" {
-  depends_on      = [null_resource.create_secret]
-  create_duration = "200s"
-}
 
 resource "rafay_download_kubeconfig" "tfkubeconfig" {
   cluster            = var.cluster_name
   output_folder_path = "/tmp"
   filename           = "kubeconfig"
 }
-
-#resource "null_resource" "install_example1" {
-#  triggers = {
-#    always_run = timestamp()
-#  }
-#  depends_on = [
-#   rafay_download_kubeconfig.tfkubeconfig,
-#   time_sleep.wait_for_service_account
-#]
-#  provisioner "local-exec" {
-#    interpreter = ["/bin/bash", "-c"]
-#    command     = "wget \"https://dl.k8s.io/release/$(wget --output-document - --quiet https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\" && chmod +x ./kubectl && ls /tmp && ./kubectl apply -f ./genai-app.yaml -n ${local.namespace} --kubeconfig=/tmp/kubeconfig && ./kubectl expose deployment gen-ai --type=LoadBalancer --name=gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig "
-#  }
-#}
 
 
 resource "rafay_workload" "install_example2" {
@@ -67,19 +49,6 @@ resource "time_sleep" "wait_for_lb" {
   depends_on      = [rafay_workload.install_example2]
   create_duration = "30s"
 }
-
-#resource "null_resource" "get-gen-ai-ip-example1" {
-#  triggers  =  { always_run = "${timestamp()}" }
-#  provisioner "local-exec" {
-#    command = "./kubectl get svc gen-ai-app-example1-lb -n ${local.namespace} --kubeconfig=/tmp/kubeconfig | awk -F' ' '{print $4}' | tail -1 | tr -d '\n' >> /tmp/gen-ai-ip-example1.txt"
-#  }
-#  depends_on = [time_sleep.wait_for_lb]
-#}
-
-#data "local_file" "gen-ai-ip-example1" {
-#    filename = "/tmp/gen-ai-ip-example1.txt"
-#  depends_on = [null_resource.get-gen-ai-ip-example1]
-#}
 
 
 resource "null_resource" "download_kubectl" {
