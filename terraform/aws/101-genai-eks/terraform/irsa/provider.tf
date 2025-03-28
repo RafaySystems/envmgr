eca599aa5cda253bf4e3b224bf8bb3739f396f91
timrafay <tim@rafay.co>
2025-03-28 13:55:01 -0400

    v1.1

--- terraform/aws/101-genai-eks/terraform/irsa/provider.tf
+++ terraform/aws/101-genai-eks/terraform/irsa/provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = var.aws_region
 }
+
+provider "kubernetes" {
+  host = var.hserver 
+  client_certificate     = base64decode(var.clientcertificatedata)
+  client_key             = base64decode(var.clientkeydata)
+  cluster_ca_certificate = base64decode(var.certificateauthoritydata)
+}
--- terraform/aws/101-genai-eks/terraform/irsa/variables.tf
+++ terraform/aws/101-genai-eks/terraform/irsa/variables.tf
@@ -10,3 +10,23 @@ variable "cluster_name" {
 variable "namespace" {
   type    = string
 }
+
+variable "hserver" {
+  description = "Kubeconfig server"
+  type        = string
+}
+
+variable "clientcertificatedata" {
+  description = "Kubeconfig client-certificate-data"
+  type        = string
+}
+
+variable "clientkeydata" {
+  description = "Kubeconfig client-key-data"
+  type        = string
+}
+
+variable "certificateauthoritydata" {
+  description = "Kubeconfig certificate-authority-data"
+  type        = string
+}
