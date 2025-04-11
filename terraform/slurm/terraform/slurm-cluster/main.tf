de6dd14cc67a092e22f6882a73c5c6568bbe1684
timrafay <91907442+timrafay@users.noreply.github.com>
2025-04-11 16:40:04 -0400

    Update main.tf

--- terraform/slurm/terraform/slurm-cluster/main.tf
+++ terraform/slurm/terraform/slurm-cluster/main.tf
@@ -2,7 +2,7 @@ resource "local_file" "slurm-cluster-values" {
   content = templatefile("${path.module}/templates/values-slurm-cluster.tftpl"), {
     storageclass   = "$var.storageclass
   })
-  filename        = ""${path.module}/values-slurm-cluster.yaml"
+  filename        = "${path.module}/values-slurm-cluster.yaml"
   file_permission = "0644"
 }
 
