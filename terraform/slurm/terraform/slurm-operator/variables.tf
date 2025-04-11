bef1b1ab50d0ddbc7b09668b98c879e670fa64a8
timrafay <91907442+timrafay@users.noreply.github.com>
2025-04-11 16:07:21 -0400

    Update variables.tf

--- terraform/slurm/terraform/slurm-operator/variables.tf
+++ terraform/slurm/terraform/slurm-operator/variables.tf
@@ -1,9 +1,1 @@
-variable "kubeconfig_path" {
-  description = "Path to the Kubernetes config file"
-  default     = "kubeconfig.json"
-}
 
-variable "rctl_config_path" {
-  description = "The path to the Rafay CLI config file"
-  type        = string
-}
