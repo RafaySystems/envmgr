d2d6337b71d8ef7e7c6d6a6563c3c4cfbe3269fc
timrafay <91907442+timrafay@users.noreply.github.com>
2025-04-15 12:38:58 -0400

    Update providers.tf

--- terraform/slurm/terraform/slurm-cluster/providers.tf
+++ terraform/slurm/terraform/slurm-cluster/providers.tf
@@ -3,3 +3,16 @@ provider "helm" {
     config_path = "kubeconfig.json"
   }
 }
+
+terraform {
+  required_providers {
+    rafay = {
+      source  = "RafaySystems/rafay"
+      version = "1.1.30"
+    }
+  }
+}
+
+provider "rafay" {
+  provider_config_file = var.rctl_config_path
+}
