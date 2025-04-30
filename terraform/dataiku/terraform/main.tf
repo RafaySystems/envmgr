23456c91e785ebc591b920e4d38500e1cc33fb6f
timrafay <91907442+timrafay@users.noreply.github.com>
2025-04-30 16:47:05 -0400

    Update main.tf

--- terraform/dataiku/terraform/main.tf
+++ terraform/dataiku/terraform/main.tf
@@ -4,6 +4,20 @@ resource "local_file" "ssh_key" {
   file_permission = "0600"
 }
 
+resource "null_resource" "create_remote_license_file" {
+  provisioner "remote-exec" {
+    inline = [
+      "echo '${var.dataiku_license}' > /home/dataiku/license.json"
+    ]
+
+    connection {
+      host        = var.remote_host
+      user        = var.remote_user
+      private_key = file(local_file.ssh_key.filename)
+    }
+  }
+}
+
 resource "null_resource" "remote_exec" {
   provisioner "file" {
     source      = "install.sh"
