621a5373f65913a257ec9eba4b50488ba30784bd
timrafay <91907442+timrafay@users.noreply.github.com>
2025-04-30 17:10:00 -0400

    Update main.tf

--- terraform/dataiku/terraform/main.tf
+++ terraform/dataiku/terraform/main.tf
@@ -4,7 +4,37 @@ resource "local_file" "ssh_key" {
   file_permission = "0600"
 }
 
+resource "null_resource" "remote_exec_prereqs" {
+  depends_on = [local_file.ssh_key]
+  provisioner "file" {
+    source      = "prereq.sh"
+    destination = "/tmp/prereq.sh"
+
+    connection {
+      type        = "ssh"
+      host        = var.remote_host
+      user        = var.remote_user
+      private_key = file(local_file.ssh_key.filename)
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/prereq.sh",
+      "sudo /tmp/prereq.sh"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = var.remote_host
+      user        = var.remote_user
+      private_key = file(local_file.ssh_key.filename)
+    }
+  }
+}
+
 resource "null_resource" "create_remote_license_file" {
+  depends_on = [null_resource.remote_exec_prereqs]
   provisioner "remote-exec" {
     inline = [
       "echo '${var.dataiku_license}' > /home/dataiku/license.json"
@@ -19,6 +49,7 @@ resource "null_resource" "create_remote_license_file" {
 }
 
 resource "null_resource" "remote_exec" {
+  depends_on = [null_resource.create_remote_license_file]
   provisioner "file" {
     source      = "install.sh"
     destination = "/tmp/install.sh"
