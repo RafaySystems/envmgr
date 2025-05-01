23f74041b833231c8fe39794ce10a2415bcc72d2
timrafay <91907442+timrafay@users.noreply.github.com>
2025-05-01 12:06:46 -0400

    Update variables.tf

--- terraform/dataiku/oci/variables.tf
+++ terraform/dataiku/oci/variables.tf
@@ -19,3 +19,20 @@ variable "ssh_public_key" {
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDArCqS9QWsOYG+CpQD/tXk0mGwUs1yVEOaCC+CRyyU6uoZ3BDv5jscCtRczc4gczNpuSt3tmajxqgA4xG7pDPTXR3vpGjBf8E8UWB1VgPOugNhcMcZ2RWkZm3jsbmAilmXpHJSzn2hA+5Lu7WYEHccViqTAMH/JEv86NU+7bqABYtG8/70UrFvCz8vco4vlEyW6mvqEZcUTxHwGHXLF4dOZgOn3h5ZBJ9oCMynX/I8yZGK0gj5EmOJzscKoqTSTdpGKXVi7FiKzYUoN7L18FwqUX92EpzbqyvRrlnldRvZAg2t04P8ZktHL8kApF+5lyRd8Dlj/WPJ7/qzgSVtrKy3 ssh-key-2022-12-06
 EOF
 }
+
+variable "remote_host" {
+  description = "Public IP or DNS of the remote server"
+  type        = string
+}
+
+variable "remote_user" {
+  description = "SSH username (e.g., ubuntu, root)"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "ssh_private_key" {
+  description = "Contents of the private SSH key"
+  type        = string
+  sensitive   = true
+}
