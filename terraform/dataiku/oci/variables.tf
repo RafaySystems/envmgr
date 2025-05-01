d3cad8401f903661870d2e69981450a22102e68d
timrafay <91907442+timrafay@users.noreply.github.com>
2025-05-01 11:37:18 -0400

    Create variables.tf

---
+++ terraform/dataiku/oci/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-phoenix-1"
+}
+
+variable "compartment_ocid" {
+  default = "ocid1.tenancy.oc1..aaaaaaaaaa3ghjcqbrbzmssbzhxzhxf24rpmuyxbaxwcj2axwoqkpd56ljkq"
+}
+
+variable "subnet_id" {
+  default = "ocid1.subnet.oc1.phx.aaaaaaaayc55km233ex74dmqpjphdeeceay4prowcc5hcoq2mj3gol3hbkda"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard3.Flex"
+}
