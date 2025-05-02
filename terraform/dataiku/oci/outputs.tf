ee38753aabf99fb8b9b10c16f90cedac7e1d0349
timrafay <91907442+timrafay@users.noreply.github.com>
2025-05-02 09:34:13 -0400

    Update outputs.tf

--- terraform/dataiku/oci/outputs.tf
+++ terraform/dataiku/oci/outputs.tf
@@ -2,9 +2,3 @@ output "instance_public_ip" {
   value = oci_core_instance.ubuntu_vm.public_ip
   description = "The public IP address of the Ubuntu instance"
 }
-
-
-output "dummy" {
-  value = "dummy"
-  description = "The public IP address of the Ubuntu instance"
-}
