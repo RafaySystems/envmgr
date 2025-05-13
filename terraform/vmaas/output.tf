2521871b07b1539ba899874bf35b0a1ff1935969
timrafay <tim@rafay.co>
2025-05-13 14:32:23 -0400

    Update output.tf

--- terraform/vmaas/output.tf
+++ terraform/vmaas/output.tf
@@ -1,3 +1,3 @@
 output "ssh_details" {
-  value = "ssh -i <Path to Private Key File> ${var.vm_username}@${vsphere_virtual_machine.controlplane.default_ip_address}"
+  value = "ssh -i <Path to Private Key File> ${var.vm_username}@${vsphere_virtual_machine.virtual_machine.default_ip_address}"
 }
