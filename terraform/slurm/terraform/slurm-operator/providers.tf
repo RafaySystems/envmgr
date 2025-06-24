41621fe9f156a18ce33c7763b61ebe9d077524ef
timrafay <91907442+timrafay@users.noreply.github.com>
2025-06-24 15:48:49 -0400

    Update providers.tf

--- terraform/slurm/terraform/slurm-operator/providers.tf
+++ terraform/slurm/terraform/slurm-operator/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.17.0"  # or the specific version you need
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "kubeconfig.json"
