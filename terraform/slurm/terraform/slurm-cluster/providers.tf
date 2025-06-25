79950bbc493b3b0dd8ba7cb793c29845ab46b04c
timrafay <91907442+timrafay@users.noreply.github.com>
2025-06-25 16:28:05 -0400

    Update providers.tf

--- terraform/slurm/terraform/slurm-cluster/providers.tf
+++ terraform/slurm/terraform/slurm-cluster/providers.tf
@@ -11,11 +11,40 @@ terraform {
   }
 }
 
+#provider "helm" {
+#  kubernetes {
+#    config_path = "kubeconfig.json"
+#  }
+#}
+
+provider "http" {}
+
+provider "local" {}
+
+# Download kubeconfig.json from URL
+data "http" "kubeconfig" {
+  url = var.kubeconfig_url
+}
+
+# Save it to a local file
+resource "local_file" "kubeconfig_file" {
+  filename = "${path.module}/kubeconfig.json"
+  content  = data.http.kubeconfig.body
+}
+
+# Wait for the file to exist (optional safety using null_resource)
+resource "null_resource" "wait_for_kubeconfig" {
+  depends_on = [local_file.kubeconfig_file]
+}
+
+# Use the kubeconfig in the Helm provider
 provider "helm" {
   kubernetes {
-    config_path = "kubeconfig.json"
+    config_path = local_file.kubeconfig_file.filename
   }
+  depends_on = [null_resource.wait_for_kubeconfig]
 }
+
 
 provider "rafay" {
   provider_config_file = var.rctl_config_path
