91a7a8d9284a0aed5b44fca1d8da5b8c26f6d7e4
timrafay <91907442+timrafay@users.noreply.github.com>
2025-06-25 16:49:55 -0400

    Update variables.tf

--- terraform/slurm/terraform/slurm-cluster/variables.tf
+++ terraform/slurm/terraform/slurm-cluster/variables.tf
@@ -14,8 +14,24 @@ variable "storageclass" {
   type = string
 }
 
-variable "kubeconfig_url" {
-  description = "URL to the kubeconfig file"
+variable "hserver" {
+  description = "Kubeconfig server"
+  type        = string
+}
+
+variable "clientcertificatedata" {
+  description = "Kubeconfig client-certificate-data"
+  type        = string
+}
+
+variable "clientkeydata" {
+  description = "Kubeconfig client-key-data"
+  type        = string
+}
+
+variable "certificateauthoritydata" {
+  description = "Kubeconfig certificate-authority-data"
+  type        = string
 }
 
 variable "rctl_config_path" {
