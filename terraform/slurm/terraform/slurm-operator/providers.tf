a41c2ed0725a8195d103fe1ca19fc8e9f42627ee
timrafay <91907442+timrafay@users.noreply.github.com>
2025-06-26 15:48:59 -0400

    Update providers.tf

--- terraform/slurm/terraform/slurm-operator/providers.tf
+++ terraform/slurm/terraform/slurm-operator/providers.tf
@@ -7,8 +7,19 @@ terraform {
   }
 }
 
+#provider "helm" {
+#  kubernetes {
+#    config_path = "kubeconfig.json"
+#  }
+#}
+
+
+
 provider "helm" {
   kubernetes {
-    config_path = "kubeconfig.json"
+    host                   = var.hserver
+    client_certificate     = base64decode(var.clientcertificatedata)
+    client_key             = base64decode(var.clientkeydata)
+    cluster_ca_certificate = base64decode(var.certificateauthoritydata)
   }
 }
