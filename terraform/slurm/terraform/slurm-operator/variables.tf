9403a2d53c002d12662de4f8505e8c92e02aa5d8
timrafay <91907442+timrafay@users.noreply.github.com>
2025-06-26 15:49:29 -0400

    Update variables.tf

--- terraform/slurm/terraform/slurm-operator/variables.tf
+++ terraform/slurm/terraform/slurm-operator/variables.tf
@@ -1,1 +1,19 @@
+variable "hserver" {
+  description = "Kubeconfig server"
+  type        = string
+}
 
+variable "clientcertificatedata" {
+  description = "Kubeconfig client-certificate-data"
+  type        = string
+}
+
+variable "clientkeydata" {
+  description = "Kubeconfig client-key-data"
+  type        = string
+}
+
+variable "certificateauthoritydata" {
+  description = "Kubeconfig certificate-authority-data"
+  type        = string
+}
