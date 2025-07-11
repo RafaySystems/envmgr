9995792839a2b9f1c05420ce88ce98e2d9aa500e
timrafay <91907442+timrafay@users.noreply.github.com>
2025-07-11 10:31:15 -0400

    Update variables.tf

--- terraform/naas/102-naas-gpu/terraform/variables.tf
+++ terraform/naas/102-naas-gpu/terraform/variables.tf
@@ -71,54 +71,6 @@ variable "namespace_quotas" {
       gpu_requests             = "3"
       gpu_limits               = "3"
     }
-    small_with_gpu = {
-      cpu_requests             = "500m"
-      memory_requests          = "1024Mi"
-      cpu_limits               = "1000m"
-      memory_limits            = "2048Mi"
-      config_maps              = "5"
-      persistent_volume_claims = "2"
-      services                 = "5"
-      pods                     = "25"
-      replication_controllers  = "1"
-      services_load_balancers  = "2"
-      services_node_ports      = "2"
-      storage_requests         = "50Gi"
-      gpu_requests             = "4"
-      gpu_limits               = "4"
-    }
-    medium_with_gpu = {
-      cpu_requests             = "1000m"
-      memory_requests          = "2048Mi"
-      cpu_limits               = "2000m"
-      memory_limits            = "4096Mi"
-      config_maps              = "10"
-      persistent_volume_claims = "4"
-      services                 = "10"
-      pods                     = "50"
-      replication_controllers  = "2"
-      services_load_balancers  = "4"
-      services_node_ports      = "4"
-      storage_requests         = "100Gi"
-      gpu_requests             = "8"
-      gpu_limits               = "8"
-    }
-    large_with_gpu = {
-      cpu_requests             = "4000m"
-      memory_requests          = "4096Mi"
-      cpu_limits               = "8000m"
-      memory_limits            = "8192Mi"
-      config_maps              = "15"
-      persistent_volume_claims = "8"
-      services                 = "20"
-      pods                     = "100"
-      replication_controllers  = "4"
-      services_load_balancers  = "8"
-      services_node_ports      = "8"
-      storage_requests         = "200Gi"
-      gpu_requests             = "16"
-      gpu_limits               = "16"
-    }
   }
 }
 
