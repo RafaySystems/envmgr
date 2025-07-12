cd4087f057943cbdc8e14283787453c47446fbd2
timrafay <91907442+timrafay@users.noreply.github.com>
2025-07-12 08:43:53 -0400

    Update output.tf

--- terraform/naas/102-naas-gpu/terraform/output.tf
+++ terraform/naas/102-naas-gpu/terraform/output.tf
@@ -6,3 +6,7 @@ output "kubeconfig" {
   description = "Rafay ZTKA kubeconfig of the cluster"
   value       = data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig
 }
+
+output "ztka" {
+  value = "https://console.rafay.dev/#/console/${var.projectid}/${var.cluster_name}?&namespace=${var.namespace}"
+}
