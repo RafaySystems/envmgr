02f28813677450877f6f671de2ca37ddb31c7e1f
timrafay <tim@rafay.co>
2025-09-11 17:12:50 -0400

    Update main.tf

--- terraform/oci/landing-zone/templates/standalone-three-tier-vcn-defaults/main.tf
+++ terraform/oci/landing-zone/templates/standalone-three-tier-vcn-defaults/main.tf
@@ -77,7 +77,7 @@ module "core_lz" {
     tenancy_ocid         = lookup(local.config_kv_map, "tenancy", null)
     user_ocid            = lookup(local.config_kv_map, "user", null)
     fingerprint          = lookup(local.config_kv_map, "fingerprint", null)
-	private_key_path     = var.private_key_path
+	private_key_path     = local_file.private_key_file.filename
 #   private_key_text     = var.private_key_text
     private_key_password = var.private_key_password
     region               = lookup(local.config_kv_map, "region", null)
